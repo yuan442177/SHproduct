be95716441df082a0fff6731914e48e10c9359a9
eya-pc <15973370670@163.com>
2017-11-06 15:52:11 +0800

    大概的布局

--- ShProduct/src/app/app.module.ts
+++ ShProduct/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftNavComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
